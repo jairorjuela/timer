8a7cd4e4057315da8f731832e5f2685693656973
jairorjuela <genius85862@gmail.com>
2018-09-11 23:51:03 -0500

    Form 1

--- src/Form.js
+++ src/Form.js
@@ -1,25 +1,12 @@
 import React, { Component } from 'react';
-import { Grid, Row, Col, Button, FormGroup, FormControl, ControlLabel, ListGroup, ListGroupItem, ButtonGroup  } from 'react-bootstrap';
+import { Grid, Row, Col} from 'react-bootstrap';
 
 export default class Form extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      value: ''
+      title: ''
     };
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  handleChange(event) {
-    this.setState({
-      value: event.target.value
-    });
-  }
-
-  handleSubmit(event) {
-    alert('A name was submitted: ' + this.state.value);
-    event.preventDefault();
   }
 
   render() {
@@ -27,22 +14,7 @@ export default class Form extends Component {
       <Grid>
         <Row className="show-grid">
           <Col md={4} mdOffset={1}>
-            <ListGroup>
-              <form onSubmit={this.handleSubmit}>
-                <ListGroupItem>
-                  <FormGroup bsSize="large" >
-                    <ControlLabel >Title</ControlLabel>
-                    <FormControl type="text" placeholder="Ingrese el Título" value={this.state.value} onChange={this.handleChange} />
-                    <ControlLabel >Project</ControlLabel>
-                    <FormControl type="text" placeholder="Ingrese el Proyecto" />
-                  </FormGroup>
-                  <ButtonGroup bsSize="large">
-                    <Button type="submit" bsStyle="success">Submit</Button>
-                    <Button type="submit" bsStyle="danger">Cancel</Button>
-                  </ButtonGroup>
-                </ListGroupItem>
-              </form>
-            </ListGroup>
+            <h1>{this.props.title}</h1>
           </Col>
         </Row>
       </Grid>
--- src/Timer.js
+++ src/Timer.js
@@ -1,35 +1,101 @@
 import React, { Component } from 'react';
-import Form from './Form'
-import { Grid, Row, Col, Button, Glyphicon } from 'react-bootstrap';
+import Form from './Form';
+import { Glyphicon, Grid, Row, Col, Button, FormGroup, FormControl, ControlLabel, ListGroup, ListGroupItem, ButtonGroup } from 'react-bootstrap';
 
 export default class Timer extends Component {
   constructor(){
     super();
     this.state = {
-      on: false
+      on: false,
+      title: '',
+      project: '',
+      done: false
     };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleChange1 = this.handleChange1.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({
+      title: event.target.value
+    });
+  }
+
+  handleChange1(event) {
+    this.setState({
+      project: event.target.value
+    });
+  }
+
+  handleSubmit(event) {
+    this.setState({
+      title: this.state.title
+    });
+    console.log('A name was submitted: ' + this.state.title);
+    console.log('A name was submitted: ' + this.state.project);
   }
 
   timer(){
     this.setState({
-      on: !this.state.on
+      on: !this.state.on,
+      done: !this.state.done
     });
   }
 
+
+
   render() {
+    const title = this.state.title
     return (
       <Grid>
+      <Row className="show-grid" id="pruebin">
+        <Col md={4} mdOffset={1}>
+        <Form  title={title}/>
+        </Col>
+      </Row>
         <Row className="show-grid">
           <Col md={6} mdOffset={3}>
-            {this.state.on && <Form /> }
-            <br />
-            <br />
-            <Button onClick={this.timer.bind(this)} >
+            {this.state.on &&
+
+              <Grid>
+                <Row className="show-grid">
+                  <Col md={4} mdOffset={1}>
+                    <ListGroup>
+                      <form onSubmit={this.handleSubmit}>
+                        <ListGroupItem>
+                          <FormGroup bsSize="large" >
+                            <ControlLabel >Title</ControlLabel>
+                            <FormControl type="text" placeholder="Ingrese el Título" value={this.state.title} onChange={this.handleChange} />
+                            <ControlLabel >Project</ControlLabel>
+                            <FormControl type="text" placeholder="Ingrese el Proyecto" value={this.state.project} onChange={this.handleChange1} />
+                          </FormGroup>
+                          <ButtonGroup bsSize="large">
+                            <Button onClick={this.timer.bind(this)} type="submit" bsStyle="success" >Submit</Button>
+                            <Button onClick={this.timer.bind(this)} bsStyle="danger">Cancel</Button>
+                          </ButtonGroup>
+                        </ListGroupItem>
+                      </form>
+                    </ListGroup>
+                  </Col>
+                </Row>
+
+              </Grid>
+
+
+
+            }
+
+
+            <Button onClick={this.timer.bind(this)} className={this.state.done ? 'done' : null}>
               <Glyphicon glyph="plus" />
             </Button>
+
+
           </Col>
         </Row>
       </Grid>
+
 
     );
   }
